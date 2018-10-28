6e3995329e5519b9a8484f5b33a1d823c8d3e913
ravindra rawat <ravinrawat.wd@gmail.com>
2018-10-28 13:30:15 -0600

    https request

--- montyPython/https_example.js
+++ montyPython/https_example.js
@@ -1,2 +1,19 @@
 const https = require('https');
 console.log('i did it');
+
+const options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+// callback called by https when request is made
+
+const callBack = () => {
+  console.log('In response handler callback');
+};
+
+console.log('i am about to make a request');
+
+https.request(options, callBack).end();
+
+console.log('i have amde the request');
